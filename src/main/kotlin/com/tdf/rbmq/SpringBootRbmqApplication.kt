6878795ee0dd9b8bee3ddb230012110a0306a0a4
Neeraj <suthar.nrj@gmail.com>
2023-12-29 20:07:39 +0530

    Init

---
+++ src/main/kotlin/com/tdf/rbmq/SpringBootRbmqApplication.kt
@@ -0,0 +1,11 @@
+package com.tdf.rbmq
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootRbmqApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootRbmqApplication>(*args)
+}
---
+++ src/test/kotlin/com/tdf/rbmq/SpringBootRbmqApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tdf.rbmq
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootRbmqApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
